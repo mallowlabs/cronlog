f4823de68b05a87fbdaabc00dcc804a6028c13b6
mallowlabs <mallowlabs@gmail.com>
2014-04-29 20:36:15 +0900

    go fmt clonlog.go

--- cronlog.go
+++ cronlog.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "os/exec"
-    "bytes"
+	"bytes"
+	"flag"
+	"fmt"
+	"os/exec"
 )
 
 func run(command []string) {
-    rest := command[1:len(command)]
-    cmd := exec.Command(command[0], rest...)
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    cmd.Stderr = &out
-    err := cmd.Run()
-    if err != nil {
-        fmt.Print(out.String())
-    } else if !cmd.ProcessState.Success() {
-        fmt.Print(out.String())
-    }
+	rest := command[1:len(command)]
+	cmd := exec.Command(command[0], rest...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	err := cmd.Run()
+	if err != nil {
+		fmt.Print(out.String())
+	} else if !cmd.ProcessState.Success() {
+		fmt.Print(out.String())
+	}
 }
 
 func main() {
-    flag.Parse()
-    run(flag.Args())
+	flag.Parse()
+	run(flag.Args())
 }
